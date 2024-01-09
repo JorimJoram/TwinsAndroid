12f252606da72eae958e0c1c23ec36e9df315094
JorimJoram <wkdgyfla97@gmail.com>
2024-01-10 06:06:18 +0900

    feat: get game List - 버튼만 넣으면 된다 - 힘들다 하...

--- app/src/main/java/twins/fan/twinsandroid/adapter/GameListAdapter.kt
+++ app/src/main/java/twins/fan/twinsandroid/adapter/GameListAdapter.kt
@@ -3,6 +3,7 @@ package twins.fan.twinsandroid.adapter
 import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,11 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import twins.fan.twinsandroid.R
 import twins.fan.twinsandroid.data.game.GameRecord
+import java.lang.StringBuilder
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.Locale
 
 class GameListAdapter(
     private val context: Context,
@@ -36,13 +42,28 @@ class GameListAdapter(
         val game = gameList!![position]
 
         val final = view.findViewById<TextView>(R.id.game_list_item_final)
+        val lgScoreSplit = game.lgScore.split(",")
+        val versusScoreSplit = game.versusScore.split(",")
 
         final.text = game.final.replace("-", "\n")
         putLogo(view, game.versusTeam, game.stadium == "잠실종합운동장")
-        //Log.d(TAG, "putScore: ${game.lgScore.split(",")}")
-        putScore(view, game.stadium == "잠실종합운동장", game.lgScore.split(","), game.versusScore.split(","))
+        putScore(view, game.stadium == "잠실종합운동장", lgScoreSplit, versusScoreSplit)
+        view.findViewById<TextView>(R.id.game_list_item_date).text = "${ dateFormat(game.gameDate.substring(0, game.gameDate.length - 1)) } ${game.startTime} ~ ${game.endTime}"
+        putPitchResult(view, game.winner, game.loser)
+
 
         return view
+    }
+
+    private fun putPitchResult(view: View, winner: String, loser: String) {
+        val homeScore = view.findViewById<TextView>(R.id.game_list_item_homeScore).text.toString().toInt()
+        val visitScore = view.findViewById<TextView>(R.id.game_list_item_visitScore).text.toString().toInt()
+        if(homeScore > visitScore){
+            view.findViewById<TextView>(R.id.game_list_item_homePitchResult).text = "승: $winner"
+            view.findViewById<TextView>(R.id.game_list_item_homePitchResult).setTextColor(Color.parseColor("#204B9B"))
+            view.findViewById<TextView>(R.id.game_list_item_visitPitchResult).text = "패: $loser"
+            view.findViewById<TextView>(R.id.game_list_item_visitPitchResult).setTextColor(Color.parseColor("#B32653"))
+        }
     }
 
     private fun putScore(view: View, isLGHome: Boolean, lgScore: List<String>, versusScore: List<String>) {
@@ -89,5 +110,22 @@ class GameListAdapter(
             .centerCrop()
             .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(view.findViewById(R.id.game_list_item_homeLogo))
+    }
+
+    private fun dateFormat(date:String): StringBuilder {
+        val result = StringBuilder()
+        for(i in date.indices step 2){
+            result.append(date.substring(i, minOf(i+2, date.length)))
+            if(i + 2 < date.length){
+                result.append(".")
+            }
+        }
+
+        val localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyMMdd"))
+        val dayOfWeekKorean = localDate.dayOfWeek.getDisplayName(TextStyle.FULL, Locale("ko-KR"))
+
+        result.append("(${dayOfWeekKorean.substring(0,1)})")
+
+        return result
     }
 }
